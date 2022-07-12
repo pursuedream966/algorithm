f323bd0fb2f3173fefbb9612f98a9cd4afa54e02
hyy <hyy_study@163.com>
2022-07-12 16:29:23 +0800

    feat(p1\sort): realization heap sort, bucket sort and radix sort

--- src/main/java/cn/pursuedream/p1/sort/SortAlgorithmMain.java
+++ src/main/java/cn/pursuedream/p1/sort/SortAlgorithmMain.java
@@ -176,9 +176,126 @@ public class SortAlgorithmMain {
     }
 
 
+    public static void heapSort(int[] arr){
+        int len = arr.length;
+
+        buildMaxHeap(arr, len);
+
+        for (int i = len - 1; i > 0; i--){
+            swap(arr, 0, i);
+            heapify(arr, 0, i);
+        }
+    }
+
+    /**
+     * 堆调整
+     * @param arr
+     * @param i
+     * @param len
+     */
+    public static void heapify(int[] arr, int i, int len){
+        int left =  2 * i + 1;
+        int right = 2 * i + 2;
+        int maxNode = i;
+
+        if(left < len && arr[left] > arr[maxNode]){
+            maxNode = left;
+        }
+
+        if(right < len && arr[right] > arr[maxNode]){
+            maxNode = right;
+        }
+
+        if(maxNode != i){
+            swap(arr, i, maxNode);
+            heapify(arr, maxNode, len);
+        }
+    }
+
+    /**
+     * 构建大顶堆
+     * @param arr
+     * @param len
+     */
+    public static void buildMaxHeap(int[] arr, int len){
+        for (int i = len / 2; i >= 0; i--){
+            heapify(arr, i, len);
+        }
+    }
+
+    /**
+     * 桶排序
+     * @param arr
+     */
+    public static void bucketSort(int[] arr){
+        int maxValue = getMaxValue(arr);
+
+        int bucketLen = maxValue + 1;
+
+        int[] bucket = new int[bucketLen];
+        for (int value : arr) {
+            bucket[value]++;
+        }
+
+        int index = 0;
+        for (int i = 0; i < bucketLen; i++){
+            while (bucket[i] > 0){
+                arr[index++] = i;
+                bucket[i]--;
+            }
+        }
+
+    }
+
+    public static int getMaxValue(int[] arr){
+        int max = arr[0];
+        for (int value : arr) {
+            if(value > max){
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public static void radixSort(int[] arr){
+        // 获取最大值
+        int maxValue = getMaxValue(arr);
+
+        // 获取最大位数
+        int maxLength = String.valueOf(maxValue).length();
+
+
+        // 构造桶
+        int[][] bucket = new int[10][arr.length];
+        int[] bucketCount = new int[bucket.length];
+
+        for (int i = 0, n = 1; i < maxLength; i++, n *= 10){
+            int index = 0;
+            for (int j = 0; j < arr.length; j++){
+                int bucketIndex = arr[j] / n % 10;
+                bucket[bucketIndex][bucketCount[bucketIndex]] = arr[j];
+                bucketCount[bucketIndex]++;
+            }
+
+            for (int k = 0; k < bucket.length; k++){
+                if(bucketCount[k] <= 0){
+                    continue;
+                }
+
+                for (int z = 0; z < bucketCount[k]; z++){
+                    arr[index++] = bucket[k][z];
+                }
+                bucketCount[k] = 0;
+            }
+
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        int[] arr = new int[]{-1, 20, 5, 345, 190,  -30, 666, 10, 20, 30, 9, 190};
+        //int[] arr = new int[]{-1, 20, 5, 345, 190,  -30, 666, 10, 20, 30, 9, 190};
+        int[] arr = new int[]{1, 20, 5, 345, 190, 30, 666, 10, 20, 30, 9, 190};
 
         System.out.println(Arrays.toString(arr));
 
@@ -200,6 +317,15 @@ public class SortAlgorithmMain {
 
         // (6) 快速排序
         // quickSort(arr, 0, arr.length-1);
+
+        // (7) 堆排序
+        // heapSort(arr);
+
+        // (8) 桶排序
+        bucketSort(arr);
+
+        // (9) 基数排序
+        radixSort(arr);
 
         System.out.println(Arrays.toString(arr));
     }
